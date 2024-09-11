d56308d7c4e82d684f5d88d6b9fcd08b29c2be96
Hitokui <46204249+Hitokui12138@users.noreply.github.com>
2024-09-11 23:35:30 +0900

    commit

--- missyou/src/main/java/com/lin/missyou/core/UnifyResponse.java
+++ missyou/src/main/java/com/lin/missyou/core/UnifyResponse.java
@@ -1,5 +1,8 @@
 package com.lin.missyou.core;
 
+import lombok.Data;
+
+@Data
 public class UnifyResponse {
     private int code;
     private String message;
@@ -8,30 +11,6 @@ public class UnifyResponse {
     public UnifyResponse(int code, String message, String request) {
         this.code = code;
         this.message = message;
-        this.request = request;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getRequest() {
-        return request;
-    }
-
-    public void setRequest(String request) {
         this.request = request;
     }
 }
